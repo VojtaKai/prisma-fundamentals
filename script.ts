ef5c7d9ab6b7ed31c257586d5b55c90dad2aeb02
Vojtech Kaiser <vksmail95@gmail.com>
2023-09-28 15:37:10 -0400

    db reads

--- script.ts
+++ script.ts
@@ -2,10 +2,15 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient() // always use only one PrismaClient for all the connections
 
 enum PrismaAction {
-    'CREATE' = 1,
-    'READ' = 2,
-    'DELETE' = 3,
-    'CREATE_MANY' = 4
+    'CREATE',
+    'READ',
+    'DELETE',
+    'CREATE_MANY',
+    'FIND_UNIQUE',
+    'FIND_UNIQUE_2',
+    'FIND_FIRST',
+    'FIND_MANY',
+    'FIND_MANY_2'
 }
 
 async function main(action: PrismaAction) {
@@ -79,7 +84,156 @@ async function main(action: PrismaAction) {
                 ],
                 skipDuplicates: true
             })
+        case PrismaAction.FIND_UNIQUE:
+            // delete everything
+            await prisma.userPreference.deleteMany()
+            await prisma.post.deleteMany()
+            await prisma.user.deleteMany()
+
+            // create new user
+            await main(PrismaAction.CREATE)
+
+            return await prisma.user.findUnique({
+                where: {
+                    email: 'vojta@gmail.com',
+                    OR: [
+                        {
+                            age: { lt: 28 }
+                        },
+                        {
+                            name: { startsWith: 'V' }
+                        }
+                    ]
+                },
+                include: {
+                    userPreference: true,
+                    favoritePosts: true,
+                    writtenPosts: true
+                }
+            })
+        case PrismaAction.FIND_UNIQUE_2:
+            // delete everything
+            await prisma.userPreference.deleteMany()
+            await prisma.post.deleteMany()
+            await prisma.user.deleteMany()
+
+            // create new user
+            await main(PrismaAction.CREATE)
+
+            return await prisma.user.findUnique({
+                where: {
+                    name_age: {
+                        name: 'Vojta',
+                        age: 28
+                    }
+                },
+                include: {
+                    userPreference: true,
+                    favoritePosts: true,
+                    writtenPosts: true
+                }
+            })
+        case PrismaAction.FIND_FIRST:
+            // delete everything
+            await prisma.userPreference.deleteMany()
+            await prisma.post.deleteMany()
+            await prisma.user.deleteMany()
+
+            // create new user
+            await main(PrismaAction.CREATE)
+
+            return await prisma.user.findFirst({
+                where: {
+                    name: 'Vojta'
+                },
+                select: {
+                    email: true,
+                    userPreference: true
+                }
+            })
+        case PrismaAction.FIND_MANY:
+            // delete everything
+            await prisma.userPreference.deleteMany()
+            await prisma.post.deleteMany()
+            await prisma.user.deleteMany()
+
+            // create new user
+            await main(PrismaAction.CREATE_MANY)
+
+            return await prisma.user.findMany({
+                where: {
+                    role: 'ADMIN'
+                },
+                include: {
+                    userPreference: true,
+                    favoritePosts: true,
+                    writtenPosts: true
+                }
+            })
+        case PrismaAction.FIND_MANY_2:
+            // delete everything
+            await prisma.userPreference.deleteMany()
+            await prisma.post.deleteMany()
+            await prisma.user.deleteMany()
+
+            // create new user
+            await main(PrismaAction.CREATE_MANY)
+            await prisma.user.createMany({
+                data: [
+                    {
+                        name: 'Vojta',
+                        age: 28,
+                        email: "vojta@gmail.com",
+                    },
+                    {
+                        name: 'Vojta',
+                        age: 40,
+                        email: "vojta40@gmail.com",
+                    },
+                    {
+                        name: 'Vojta',
+                        age: 13,
+                        email: "vojta13@gmail.com",
+                    }
+                ]
+            })
+
+            return await prisma.user.findMany({
+                // where: {
+                //     AND: [
+                //         {
+                //             email: { endsWith: '@gmail.com' },
+                //             name: { in: ['Vojta', 'Peter'] }
+                //         },
+                //         {
+                //             email: { contains: 'vojta' }
+                //         },
+                //     ]
+                // },
+                // where: {
+                //     OR: [
+                //         {
+                //             email: { endsWith: '@gmail.com' }
+                //         },
+                //         {
+                //             email: { contains: 'vojta' }
+                //         },
+                //     ]
+                // },
+                where: {
+                    NOT: {
+                        email: { endsWith: '@gmail.com' }
+                    }
+                },
+                distinct: ["name", "age"],
+                // take: 2, // pagination
+                // skip: 1, // will skip first N-count
+                // cursor: // LastEvaluatedKey in dynamoDB but you have to create it manually - take unique prop - id / unique field of the last returned item, always works with take 
+                orderBy: {
+                    age: 'desc'
+                }
+            })
     }
 }
 
-main(PrismaAction.CREATE_MANY).then((resp) => console.log('success', resp)).catch(err => console.log('error', err)).finally(() => prisma.$disconnect())
+main(PrismaAction.FIND_MANY_2).then((resp) => console.log('success', resp)).catch(err => console.log('error', err)).finally(() => prisma.$disconnect())
